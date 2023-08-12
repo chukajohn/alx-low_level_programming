4863963f280c8343d591c204674ef4806882cfe6
chukajohn <chukajohn4@gmail.com>
2023-08-12 09:46:47 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - Is the function for the programme
+* Return: 0 when is sucessful
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
