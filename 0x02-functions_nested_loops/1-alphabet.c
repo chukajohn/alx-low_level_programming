4d3c299007b32d2786b6dbc0070405cc848d5c8e
chukajohn <chukajohn4@gmail.com>
2023-08-16 03:34:27 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 1-main.c 1-alphabet.c -o 1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
-* main - function that print alphabet in lower case
+* print_alphabet - a function that prints the alphabet, in lowercase
 *
 * Return: always return 0 when the code nis successful
-* print_alphabet - a prototype function
 */
 
 void print_alphabet(void)
