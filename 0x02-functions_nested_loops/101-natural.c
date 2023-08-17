dc0a3b4415eaf7d6c33232223621d4dee5ea7638
chukajohn <chukajohn4@gmail.com>
2023-08-17 01:39:52 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - the function
+*/
+int main(void)
+{
+	int sum = 0;
+
+	int i;
+
+	for (i = 1; 1 <= 10; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum = sum + i;
+	}
+
+	printf("%d\n", sum);
+}
+
