d6b7e6e05977272529a6ba884c501fed4fcb49a2
chukajohn <chukajohn4@gmail.com>
2023-08-18 09:57:23 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,11 +14,13 @@ void more_numbers(void)
 	for (i = 0; i < 10; i++)
 	{
 		for (k = 0; k <= 14; k++)
+
 		{
-			_putchar(k + '0');
+			if (k > 9)
+				_putchar(k / 10 + '0');
+			_putchar(k % 10 + '0');
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 
 }
