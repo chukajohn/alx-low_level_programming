dada5fdf822d188acbd4134c05ba905175aada30
chukajohn <chukajohn4@gmail.com>
2023-08-18 20:19:05 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* print_number - main function
+* @n: parameter to be checked
+* Return: always 0
+*/
+
+void print_number(int n)
+{
+	int i;
+
+	for (i = 0;  i < 10; i++)
+		_putchar(i + '0');
+}
