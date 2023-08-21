932f7eaa4528b82839998826a9a1cf87bbb2db44
chukajohn <chukajohn4@gmail.com>
2023-08-21 08:10:12 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _strlen -  a function that returns the length of a string
+* @s:  string to be counted
+* Return: always 0
+*/
+
+int _strlen(char *s)
+{
+	int i;
+	int counting = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		counting++;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 
 int _putchar(char c);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 
 #endif
 
