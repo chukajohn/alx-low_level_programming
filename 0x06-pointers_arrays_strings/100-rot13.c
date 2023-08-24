0db1d6a01e8f051551ea267c7646870321b2a6cb
chukajohn <chukajohn4@gmail.com>
2023-08-24 02:56:30 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* rot13 -  a function that encodes a string using rot13.
+*@s: string to br encoded
+* Return: always 0
+*/
+
+char *rot13(char *s)
+{
+	int i;
+	char j;
+	char *a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *b = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
+	}
+return (s);
+
+}
