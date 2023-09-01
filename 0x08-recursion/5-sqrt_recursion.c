689b43cc62734d2975064ec04776b6ca7a7b29eb
chukajohn <chukajohn4@gmail.com>
2023-08-31 18:44:32 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,7 +4,6 @@
  * sqrt_g - function for the sqaure root
  * @a: parameters
  * @b: iterator
- * _sqrt_recursion - a function that returns a natural root of a number
  * Return: always 0
  */
 
@@ -14,12 +13,19 @@ int sqrt_g(int a, int b)
 	{
 		return (b);
 	}
-	if (b * b > 4)
+	if (b * b > a)
 	{
 		return (-1);
 	}
 	return (sqrt_g(a, b + 1));
 }
+
+
+/**
+ * _sqrt_recursion - a function that returns square root
+ * @n: parameter to be passed
+ * Return: always 0
+ */
 
 int _sqrt_recursion(int n)
 {
