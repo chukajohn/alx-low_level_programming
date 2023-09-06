6d51a13113b8d6029c3761462a32dcda104a5f40
chukajohn <chukajohn4@gmail.com>
2023-09-06 03:37:52 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,10 +15,10 @@ char *str_concat(char *s1, char *s2)
 	char *out;
 
 	if (s1 == NULL)
-		s1 = "";
+		return (s1 = "");
 
 	if (s2 == NULL)
-		s2 = "";
+		return (s2 = "");
 
 	for (j = 0; s1[j] != '\0'; j++)
 		s1len++;
