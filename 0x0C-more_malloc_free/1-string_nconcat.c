babc68b0c5093bc52567bdaa582e22eeabb129ad
chukajohn <chukajohn4@gmail.com>
2023-09-09 03:00:50 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,6 @@
-#include "main.h"
 #include <stdlib.h>
-
+#include "main.h"
+#include <stdio.h>
 /**
  * string_nconcat -  a function that concatenates two strings
  * @s1: string to be concanteneated
@@ -12,33 +12,40 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	int i;
+	int s1len = 0;
+	int s2len = 0;
 	char *s;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	while (s1 && s1[len1])
-		len1++;
-	while (s2 && s2[len2])
-		len2++;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-	if (n < len2)
-		s = malloc(sizeof(char) * (len1 + n + 1));
-	else
-		s = malloc(sizeof(char) * (len1 + len2 + 1));
+	for (i = 0; s1[i] != '\0'; i++)
+		s1len++;
+	for (i = 0; s2[i] != '\0'; i++)
+		s2len++;
 
-	if (!s)
+	s = malloc(sizeof(char) * (s1len + n) + 1);
+	if (s == NULL)
 		return (NULL);
-
-		while (i < len1)
-		{
+	if (n >= s2len)
+	{
+		for (i = 0; s1[i] != '\0'; i++)
 			s[i] = s1[i];
-			i++;
-		}
+		for (i = 0; s2[i] != '\0'; i++)
 
-		while (n < len2 && i < (len1 + n))
-			s[i++] = s2[j++];
-		while (n >= len2 && i < (len1 + len2))
-			s[i++] = s2[j++];
-		s[i] = '\0';
-
+			s[s1len + i] = s2[i];
+		s[s1len + i] = '\0';
+	}
+	else
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+			s[i] = s1[i];
+		for (i = 0; i < n; i++)
+			s[s1len + i] = s2[i];
+		s[s1len + i] = '\0';
+	}
 		return (s);
 }
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _memset - function that fills memory with a byte
+ * @s: memory area to be filled
+ *
+ *
+ */
+
+
+
+
+
+/**
+ * _calloc - a function that allocates memory for an array, using malloc
+ * 
+ *
+ *
+ */
