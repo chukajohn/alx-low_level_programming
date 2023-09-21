65b7d2a4dccf369ff216810b18561979bb46633f
chukajohn <chukajohn4@gmail.com>
2023-09-21 12:03:26 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,12 @@
 #include "lists.h"
 /**
- * myStaetupFun - implementation of myfunction
+ * myStartupFun - implementation of myfunction
+ * Return: 0
  */
 
 int myStartupFun(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
+	return (0);
 }
