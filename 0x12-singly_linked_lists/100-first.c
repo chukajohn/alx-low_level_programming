85a1f421c63edd0e9838696de1a6e1d790306168
chukajohn <chukajohn4@gmail.com>
2023-09-21 11:51:55 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,7 @@
  * myStaetupFun - implementation of myfunction
  */
 
-void myStartupFun(void)
+int myStartupFun(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -19,7 +19,7 @@ typedef struct list_s
 } list_t;
 
 int _putchar(char c);
-void myStartupFun(void);
+int myStartupFun(void);
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
