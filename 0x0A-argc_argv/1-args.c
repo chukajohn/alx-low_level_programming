0895a85465a1701c13917d8e9920b9de810aa436
chukajohn <chukajohn4@gmail.com>
2023-10-29 08:54:47 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - progame that prints the numb
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: 0 always on success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
