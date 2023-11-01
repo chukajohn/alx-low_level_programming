ec7f23eb1a9787a5c5fd955024582613fd3e19e2
chukajohn <chukajohn4@gmail.com>
2023-10-31 19:22:03 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *_strdup - funtion that returns a pointer
+ *@str: the string
+ *
+ *Return: a pointer
+ */
+char *_strdup(char *str)
+{
+	size_t i, k;
+	char *strd;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		strd = malloc(sizeof(char) * (i + 1));
+	if (strd == NULL)
+		return (NULL);
+	for (k = 0; k <= i; k++)
+	{
+		strd[k] = str[k];
+	}
+	return (strd);
+}
