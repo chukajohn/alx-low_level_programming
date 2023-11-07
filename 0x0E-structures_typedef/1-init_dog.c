f9d9ca837b1915e9e83966fc19e383aef7dccae4
chukajohn <chukajohn4@gmail.com>
2023-11-06 20:07:04 -0800

    dog.h file

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+
+/**
+ *init_dog - a duncrion that init variables
+ *@age: age of the owner
+ *@name: name of the owner
+ *@owner: of the variable
+ *
+ */
+'
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - funcrion
+ *@name: name of the owner
+ *@owner: onwers name
+ *@age: age of the owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+
+#endif
