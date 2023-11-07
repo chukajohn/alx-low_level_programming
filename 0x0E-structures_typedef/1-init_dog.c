63c2914f2f52127f199e22b1222b2d621f86dd6c
chukajohn <chukajohn4@gmail.com>
2023-11-06 20:23:47 -0800

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,14 +1,19 @@
 #include "dog.h"
-
+#include <stddef.h>
 /**
  *init_dog - a duncrion that init variables
  *@age: age of the owner
  *@name: name of the owner
  *@owner: of the variable
- *
+ *@d: new variable
  */
-'
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
