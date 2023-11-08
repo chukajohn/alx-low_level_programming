99b4393bfe50d68178f3fd8db10cfaec82467ef6
chukajohn <chukajohn4@gmail.com>
2023-11-07 17:40:48 -0800

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ *print_dog - function that prints dog
+ *@d: a pointer to the vrible
+ *
+ *
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("name: %s\n", (d->name) ? d->name : "(nil)");
+		printf("age: %f\n", (d->age) ? d->age : 0);
+		printf("owner: %s\n", (d->owner) ? d->owner : "(nil)");
+	}
+}
