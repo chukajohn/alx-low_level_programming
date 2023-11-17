af31f2de89ea24de523f9a060ae600c367da45a9
chukajohn <chukajohn.com>
2023-11-17 08:10:39 -0800

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,2 +1,18 @@
 #include "lists.h"
+/**
+ *list_len - prints the number of elemrnt
+ *@h: head node
+ *
+ *Return: node
+ */
+size_t list_len(const list_t *h)
+{
+	size_t nodes;
 
+	while (h != NULL)
+	{
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
+}
