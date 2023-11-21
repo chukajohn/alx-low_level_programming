320daa64aa8333f0e716d2df72370a8c3a9e2455
chukajohn <chukajohn.com>
2023-11-20 18:33:27 -0800

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,12 +9,13 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *newnode;
+
 	if (head == NULL)
 		return;
 
 	while (*head != NULL)
 	{
-		newnode->next = *head;
+		newnode = (*head)->next;
 		free(*head);
 		*head = newnode;
 	}
