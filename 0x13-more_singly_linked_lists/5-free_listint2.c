1cbfa35851d086e7d524b9e691d8ba22cb4435f9
chukajohn <chukajohn.com>
2023-11-20 18:29:28 -0800

    free_listint2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ *free_listint2 - function that frees listint_t list
+ *@head: head pointeer
+ *Return: head
+ *
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *newnode;
+	if (head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		newnode->next = *head;
+		free(*head);
+		*head = newnode;
+	}
+	head = NULL;
+}
