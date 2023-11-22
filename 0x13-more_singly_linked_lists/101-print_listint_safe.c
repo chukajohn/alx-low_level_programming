4faaaf8f8995543eff4fd662308c851446e1060e
chukajohn <chukajohn.com>
2023-11-21 21:21:44 -0800

    pop_listint

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -50,7 +50,7 @@ size_t print_listint_safe(const listint_t *head)
 		while (add->next != NULL)
 		{
 			add = add->next;
-			if (head == add)
+			if (head == add->p)
 			{
 				printf("-> [%p] %d\n", (void *)head, head->n);
 				free_listp(&ptr);
--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,8 +1,21 @@
-#include "list.h"
+#include "lists.h"
 /**
- *
- *
+ * pop_listint - deletes the head node
+ *@head: pointer head
+ *Return: 0
  *
  *
  */
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *tmp;
 
+	if (head == NULL || *head == NULL)
+		return (0);
+	n = (*head)->n;
+	tmp = *head;
+	*head = (*head)->next;
+	free(tmp);
+	return (n);
+}
