5540684506e53186fbb4e700a53553a63743cbb8
chukajohn <chukajohn.com>
2023-12-01 04:59:46 -0800

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,19 +15,16 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file_d = fopen(filename, O_WRONLY | O_APPEND);
-
-	if (file_d == -1)
-		return (-1);
-
-	if (text_content)
+	if (text_content == NULL)
 	{
-		for (n = 0; text_content[n]; n++)
-		wr = write(file_d, text_content, n);
-
-		if (wr == -1)
-			return (-1);
+	for (n = 0; text_content[n];)
+		n++;
 	}
+
+	file_d = open(filename, O_WRONLY | O_APPEND);
+
+	if (file_d == -1 || wr == -1)
+		return (-1);
 
 
 	close(file_d);
